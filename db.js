f72f86fe0f6d52e504a7270a0f079e3b5ee70b20
MMS-learning-math <129403810+MMS-learning-math@users.noreply.github.com>
2025-08-14 18:24:30 +0000

    Changes to be committed:

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "root",
+  password: "password123",
+  database: "temp",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
